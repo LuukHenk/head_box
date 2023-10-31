cf29707f9ce281f905a486484ddf1095596f4d74
LuukHenk <luukperdaems@hotmail.com>
2023-10-31 14:17:11 +0100

    decoupled the bullet_bundle from the rigid body bundle

--- src/src/in_game/data_layers/bullet_bundle.rs
+++ src/src/in_game/data_layers/bullet_bundle.rs
@@ -2,13 +2,10 @@ use std::time::Duration;
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
-use crate::in_game::data_classes::rigid_body_constants::{DEFAULT_ACTIVE_EVENTS, DEFAULT_GRAVITY, DEFAULT_VELOCITY, DEFAULT_WALING_VELOCITY};
-use crate::in_game::data_classes::bullet_constants::{BULLET_LENGTH, BULLET_WIDTH, SHOOTER_DISTANCE_BUFFER};
+use crate::in_game::data_classes::rigid_body_constants::{DEFAULT_ACTIVE_EVENTS, DEFAULT_GRAVITY, DEFAULT_VELOCITY};
+use crate::in_game::data_classes::bullet_constants::{BULLET_LENGTH, BULLET_WIDTH};
 use crate::in_game::data_classes::bullet_components::{BulletMarker, Damage, LifeTime};
 use crate::in_game::data_classes::generic_components::GameScreenMarker;
-use crate::generic_constants::{Z_VALUE};
-
-use super::rigid_body_bundle::RigidBodyBundle;
 
 
 #[derive(Bundle)]
@@ -16,8 +13,16 @@ pub struct BulletBundle {
     bullet_marker: BulletMarker,
     damage: Damage,
     life_time: LifeTime,
-    rigid_body_bundle: RigidBodyBundle,
     game_screen_marker: GameScreenMarker,
+    rigid_body: RigidBody,
+    sprite_bundle: SpriteBundle,
+    velocity: Velocity,
+    gravity: GravityScale,
+    collider: Collider,
+    continuous_collision_detection: Ccd,
+    sleeping: Sleeping,
+    collision_groups: CollisionGroups,
+    active_events: ActiveEvents,
 }
 
 impl BulletBundle {
@@ -26,11 +31,17 @@ impl BulletBundle {
         collision_groups: CollisionGroups,
         texture: Handle<Image>,
     ) -> BulletBundle {
-        let bullet_rigid_body = RigidBodyBundle {
+
+        let bullet_timer = Timer::new(Duration::from_secs_f32(0.1), TimerMode::Once);
+
+        BulletBundle {
+            damage: Damage(0.5),
+            life_time: LifeTime(bullet_timer),
+            bullet_marker: BulletMarker,
+            game_screen_marker: GameScreenMarker,
             rigid_body: RigidBody::Fixed,
             velocity: DEFAULT_VELOCITY,
             gravity: DEFAULT_GRAVITY,
-            walking_velocity: DEFAULT_WALING_VELOCITY,
             collider: Collider::cuboid(BULLET_WIDTH, BULLET_LENGTH),
             continuous_collision_detection: Ccd::disabled(),
             sprite_bundle: SpriteBundle {
@@ -41,16 +52,6 @@ impl BulletBundle {
             sleeping: Sleeping::disabled(),
             collision_groups,
             active_events: DEFAULT_ACTIVE_EVENTS,
-        };
-
-        let bullet_timer = Timer::new(Duration::from_secs_f32(0.1), TimerMode::Once);
-
-        BulletBundle {
-            damage: Damage(0.5),
-            life_time: LifeTime(bullet_timer),
-            bullet_marker: BulletMarker,
-            rigid_body_bundle: bullet_rigid_body,
-            game_screen_marker: GameScreenMarker,
         }
     }
 }
