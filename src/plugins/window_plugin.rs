237e87e911a8d2b7bed6d21e29e633246c3b47f9
Luuk Perdaems <LuukPerdaems@hotmail.com>
2025-02-24 20:57:53 +0100

    n00b

--- src/plugins/window_plugin.rs
+++
@@ -1,8 +0,0 @@
-.add_plugins(DefaultPlugins.set(WindowPlugin {
-    primary_window: Some(Window {
-        title: "HeadBox".into(),
-        resolution: (1920., 1080.).into(),
-        ..default()
-    }),
-    ..default()
-}))
