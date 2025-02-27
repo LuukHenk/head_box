f47f22112532e049be48b81a92e2a8327a03b7a0
Luuk Perdaems <LuukPerdaems@hotmail.com>
2025-02-27 12:17:51 +0100

    test world

--- src/game/plugin.rs
+++ src/game/plugin.rs
@@ -8,6 +8,7 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(GameState::Game), setup);
+        app.add_systems(OnEnter(GameState::Game), setup_test_world); // Replace with plugin
         app.add_systems(OnExit(GameState::Game), despawn_entities::<OnGameScreen>);
         app.add_plugins(PlayerPlugin);
     }
@@ -21,4 +22,16 @@ fn setup(mut commands: Commands) {
 }
 fn create_camera() -> (Camera2d, Camera) {
     (Camera2d, Camera { ..default() })
+}
+
+fn setup_test_world(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn((
+        OnGameScreen,
+        Mesh2d(meshes.add(Rectangle::new(1000., 700.))),
+        MeshMaterial2d(materials.add(Color::srgb(0.2, 0.2, 0.3))),
+    ));
 }
